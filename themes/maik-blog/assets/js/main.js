c4785c3f7e0eda35cc63cbba1838c8b3e8e29427
Maik de Kruif <maik@maik.dev>
2024-11-21 17:16:03 +0100

    Add functionality to enlarge figure by clicking on it

--- themes/maik-blog/assets/js/main.js
+++ themes/maik-blog/assets/js/main.js
@@ -1,1 +1,20 @@
-// Some code could be here ...
+window.addEventListener("load", () => {
+    for (let figure of document.getElementsByTagName("figure")) {
+        if (figure.classList.contains("post-cover"))
+            continue
+
+        figure.addEventListener("click", () => {
+            figure.classList.toggle("floated-focus")
+        })
+    }
+
+    document.addEventListener('keydown', (e) => {
+        if (e.keyCode !== 27)
+            return
+
+        for (let figure of document.getElementsByClassName("floated-focus")) {
+            figure.classList.remove("floated-focus")
+        }
+
+    })
+})
