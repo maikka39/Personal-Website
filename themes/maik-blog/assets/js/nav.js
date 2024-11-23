c859780f74f3284b1f8c11836db115f9220c1145
Maik de Kruif <maik@maik.dev>
2024-11-23 17:31:14 +0100

    Use 4 level indentation everywhere

--- themes/maik-blog/assets/js/nav.js
+++ themes/maik-blog/assets/js/nav.js
@@ -1,38 +1,40 @@
+// Path navigation on top left
 window.addEventListener("load", () => {
-  const terminal = document.getElementById('nav-terminal')
+    const terminal = document.getElementById("nav-terminal");
 
-  const keyDown = (e) => {
-    if (e.keyCode === 13) {
-      if (document.activeElement !== document.body) return
-      if (terminal.textContent.length === 0) return
+    const keyDown = (e) => {
+        if (e.keyCode === 13) {
+            if (document.activeElement !== document.body) return;
+            if (terminal.textContent.length === 0) return;
 
-      if (terminal.textContent.includes("~")) terminal.textContent = ""
+            if (terminal.textContent.includes("~")) terminal.textContent = "";
 
-      window.location.href = window.location.origin + "/" + terminal.textContent
-      return
-    }
+            window.location.href =
+                window.location.origin + "/" + terminal.textContent;
+            return;
+        }
 
-    if (e.keyCode === 8) {
-      terminal.textContent = terminal.textContent.slice(0, -1)
-      return
-    }
+        if (e.keyCode === 8) {
+            terminal.textContent = terminal.textContent.slice(0, -1);
+            return;
+        }
 
-    if (e.keyCode === 192) {
-      if (terminal.textContent.length !== 0) return
+        if (e.keyCode === 192) {
+            if (terminal.textContent.length !== 0) return;
 
-      terminal.textContent += "~"
-      return
-    }
+            terminal.textContent += "~";
+            return;
+        }
 
-    if (e.keyCode === 191) {
-      terminal.textContent += "/"
-      return
-    }
+        if (e.keyCode === 191) {
+            terminal.textContent += "/";
+            return;
+        }
 
-    if (e.keyCode < 65 || e.keyCode > 90) return
+        if (e.keyCode < 65 || e.keyCode > 90) return;
 
-    terminal.textContent += e.key.toLowerCase()
-  }
+        terminal.textContent += e.key.toLowerCase();
+    };
 
-  document.addEventListener('keydown', keyDown)
-})
+    document.addEventListener("keydown", keyDown);
+});
--- themes/maik-blog/assets/js/theme.js
+++ themes/maik-blog/assets/js/theme.js
@@ -6,20 +6,20 @@ const isDark = theme === "dark";
 var metaThemeColor = document.querySelector("meta[name=theme-color]");
 
 if (theme !== null) {
-  document.body.classList.toggle("dark-theme", isDark);
-  isDark
-    ? metaThemeColor.setAttribute("content", "#252627")
-    : metaThemeColor.setAttribute("content", "#fafafa");
+    document.body.classList.toggle("dark-theme", isDark);
+    isDark
+        ? metaThemeColor.setAttribute("content", "#252627")
+        : metaThemeColor.setAttribute("content", "#fafafa");
 }
 
 themeToggle.addEventListener("click", () => {
-  document.body.classList.toggle("dark-theme");
-  window.localStorage &&
-    window.localStorage.setItem(
-      "theme",
-      document.body.classList.contains("dark-theme") ? "dark" : "light"
-    );
-  document.body.classList.contains("dark-theme")
-    ? metaThemeColor.setAttribute("content", "#252627")
-    : metaThemeColor.setAttribute("content", "#fafafa");
+    document.body.classList.toggle("dark-theme");
+    window.localStorage &&
+        window.localStorage.setItem(
+            "theme",
+            document.body.classList.contains("dark-theme") ? "dark" : "light"
+        );
+    document.body.classList.contains("dark-theme")
+        ? metaThemeColor.setAttribute("content", "#252627")
+        : metaThemeColor.setAttribute("content", "#fafafa");
 });
