54cb434cd3514a870261b96af2d404b109a43794
Maik de Kruif <maik@maik.dev>
2024-11-25 16:37:09 +0100

    Increase dark mode contrast

--- themes/maik-blog/assets/js/theme.js
+++ themes/maik-blog/assets/js/theme.js
@@ -8,8 +8,8 @@ var metaThemeColor = document.querySelector("meta[name=theme-color]");
 if (theme !== null) {
     document.body.classList.toggle("dark-theme", isDark);
     isDark
-        ? metaThemeColor.setAttribute("content", "#252627")
-        : metaThemeColor.setAttribute("content", "#fafafa");
+        ? metaThemeColor.setAttribute("content", "#1b1c1c")
+        : metaThemeColor.setAttribute("content", "#f9f9f9");
 }
 
 themeToggle.addEventListener("click", () => {
@@ -20,6 +20,6 @@ themeToggle.addEventListener("click", () => {
             document.body.classList.contains("dark-theme") ? "dark" : "light"
         );
     document.body.classList.contains("dark-theme")
-        ? metaThemeColor.setAttribute("content", "#252627")
-        : metaThemeColor.setAttribute("content", "#fafafa");
+        ? metaThemeColor.setAttribute("content", "#1b1c1c")
+        : metaThemeColor.setAttribute("content", "#f9f9f9");
 });
